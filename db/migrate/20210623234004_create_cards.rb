848de6f57299d59eb8ad4b7747bbb95d12945a6a
Andrew-CC-Martin <amartin262@gmail.com>
2021-06-24 15:11:46 +1000

    create cards scaffold

--- db/migrate/20210623234004_create_cards.rb
+++ db/migrate/20210623234004_create_cards.rb
@@ -3,7 +3,7 @@ class CreateCards < ActiveRecord::Migration[6.1]
     create_table :cards do |t|
       t.string :title
       t.text :description
-      t.boolean :completed
+      t.boolean :completed, :default => false
 
       t.timestamps
     end
