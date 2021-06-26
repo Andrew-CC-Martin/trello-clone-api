c88ca2833aca3e2052d0b0cb615152da5496f825
Andrew-CC-Martin <amartin262@gmail.com>
2021-06-26 16:14:55 +1000

    remove CORS wildcard & add in whitelist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000', 'https://frozen-cliffs-40033.herokuapp.com'
 
     resource '*',
       headers: :any,
