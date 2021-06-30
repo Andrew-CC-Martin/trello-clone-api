03c65cde241e089e3f82d97ca8d663da500d3997
Andrew-CC-Martin <amartin262@gmail.com>
2021-06-30 13:40:20 +1000

    set up sign up and sign in with knock gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :cards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope '/auth' do
+    post 'sign_up', to: 'users#create'
+    post 'sign_in', to: 'users#sign_in'
+  end
 end
