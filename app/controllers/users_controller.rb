a3a85dd8b4ccbd96e4e0498c91f509d1025a99d9
Andrew-CC-Martin <amartin262@gmail.com>
2021-07-01 11:11:47 +1000

    create user model and create/sign up endpoints

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+      render json: {username: @user.username, jwt: auth_token.token}, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def sign_in
+    @user = User.find_by_email(params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+      render json: {username: @user.username, jwt: auth_token.token}, status: 200
+    else
+      render json: {error: "incorrect username or password"}, status: 404
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
+end
