e5950da942c26a1f632918c76f0c3ca6c40a34fc
Andrew-CC-Martin <amartin262@gmail.com>
2021-07-03 16:29:11 +1000

    ad wildcard * to cors config, so that netlify preview deployments can be used

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://dreamy-dubinsky-916568.netlify.app'
+    origins 'http://localhost:3000', '*dreamy-dubinsky-916568.netlify.app'
 
     resource '*',
       headers: :any,
